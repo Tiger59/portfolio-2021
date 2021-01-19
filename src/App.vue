802e55206ec713b60e99380a9eb341a2039d6346
Tiger59 <kuroneko59@icloud.com>
2021-01-19 22:15:14 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" width=200px src="./assets/koji.png">
+    <HelloWorld msg="工事中"/>
   </div>
 </template>
 
